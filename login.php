c8ade8e18c1b3076176a0340d1244bdf82b9ba2f
shudanti <shudanti@gmail.com>
2015-06-22 14:14:49 +0700

    121

--- login.php
+++ login.php
@@ -1,7 +1,5 @@
 <?php
 include("config.php");
-
-error_reporting(E_ALL ^ E_NOTICE);
 $r_name=$_REQUEST['r_name'];
 $r_email=$_REQUEST['r_email'];
 $r_password=$_REQUEST['r_password'];
@@ -10,19 +8,41 @@ $r_btsubmit=$_REQUEST['r_btsubmit'];
 if(isset($_POST['r_btsubmit']))
 {
 
-$sel=mysql_query("select email from users where email = '$r_email' ");
+$sel=mysql_query("select name from users where name = '$r_name' ");
 $arr=mysql_fetch_array($sel);
 
-if($arr['email']!=$r_email)
+if($arr['name']!=$r_name)
   {
    if(mysql_query("insert into users values('$r_name','$r_password','$r_email')"))
 	   {  
-	     echo "New users created successfully";
+	       
+	       echo "<script>alert('New users created successfully')</script>";
 	   }
 	 }
 else 
 {
 	echo "user already exists";
+}
+
+}
+$name=$_REQUEST['name'];
+$password=$_REQUEST['password'];
+$btsubmit=$_REQUEST['btsubmit'];
+if(isset($_POST['btsubmit']))
+{
+
+$sel=mysql_query("select * from users where name = '$name' and password = '$password' ");
+$arr=mysql_fetch_array($sel);
+
+if($arr['name']==$name)
+  {
+        session_start();
+        $_REQUEST['id_name'] = $name;
+        echo "<script>alert('Users login successfully')</script>";
+	 }
+else 
+{
+	echo "Login false";
 }
 
 }
@@ -54,138 +74,24 @@ else
 </head><!--/head-->
 
 <body>
-	<header id='header'><!--header-->
-		<div class='header_top'><!--header_top-->
-			<div class='container'>
-				<div class='row'>
-					<div class='col-sm-6'>
-						<div class='contactinfo'>
-							<ul class='nav nav-pills'>
-								<li><a href=''><i class='fa fa-phone'></i> +2 95 01 88 821</a></li>
-								<li><a href=''><i class='fa fa-envelope'></i> info@domain.com</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-6'>
-						<div class='social-icons pull-right'>
-							<ul class='nav navbar-nav'>
-								<li><a href=''><i class='fa fa-facebook'></i></a></li>
-								<li><a href=''><i class='fa fa-twitter'></i></a></li>
-								<li><a href=''><i class='fa fa-linkedin'></i></a></li>
-								<li><a href=''><i class='fa fa-dribbble'></i></a></li>
-								<li><a href=''><i class='fa fa-google-plus'></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header_top-->
-		
-		<div class='header-middle'><!--header-middle-->
-			<div class='container'>
-				<div class='row'>
-					<div class='col-sm-4'>
-						<div class='logo pull-left'>
-							<a href='index.html'><img src='images/home/logo.png' alt='' /></a>
-						</div>
-						<div class='btn-group pull-right'>
-							<div class='btn-group'>
-								<button type='button' class='btn btn-default dropdown-toggle usa' data-toggle='dropdown'>
-									USA
-									<span class='caret'></span>
-								</button>
-								<ul class='dropdown-menu'>
-									<li><a href=''>Canada</a></li>
-									<li><a href=''>UK</a></li>
-								</ul>
-							</div>
-							
-							<div class='btn-group'>
-								<button type='button' class='btn btn-default dropdown-toggle usa' data-toggle='dropdown'>
-									DOLLAR
-									<span class='caret'></span>
-								</button>
-								<ul class='dropdown-menu'>
-									<li><a href=''>Canadian Dollar</a></li>
-									<li><a href=''>Pound</a></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div class='col-sm-8'>
-						<div class='shop-menu pull-right'>
-							<ul class='nav navbar-nav'>
-								<li><a href=''><i class='fa fa-user'></i> Account</a></li>
-								<li><a href=''><i class='fa fa-star'></i> Wishlist</a></li>
-								<li><a href='checkout.html'><i class='fa fa-crosshairs'></i> Checkout</a></li>
-								<li><a href='cart.html'><i class='fa fa-shopping-cart'></i> Cart</a></li>
-								<li><a href='login.html' class='active'><i class='fa fa-lock'></i> Login</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header-middle-->
 	
-		<div class='header-bottom'><!--header-bottom-->
-			<div class='container'>
-				<div class='row'>
-					<div class='col-sm-9'>
-						<div class='navbar-header'>
-							<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-								<span class='sr-only'>Toggle navigation</span>
-								<span class='icon-bar'></span>
-								<span class='icon-bar'></span>
-								<span class='icon-bar'></span>
-							</button>
-						</div>
-						<div class='mainmenu pull-left'>
-							<ul class='nav navbar-nav collapse navbar-collapse'>
-								<li><a href='index.html'>Home</a></li>
-								<li class='dropdown'><a href='#'>Shop<i class='fa fa-angle-down'></i></a>
-                                    <ul role='menu' class='sub-menu'>
-                                        <li><a href='shop.html'>Products</a></li>
-										<li><a href='product-details.html'>Product Details</a></li> 
-										<li><a href='checkout.html'>Checkout</a></li> 
-										<li><a href='cart.html'>Cart</a></li> 
-										<li><a href='login.html' class='active'>Login</a></li> 
-                                    </ul>
-                                </li> 
-								<li class='dropdown'><a href='#'>Blog<i class='fa fa-angle-down'></i></a>
-                                    <ul role='menu' class='sub-menu'>
-                                        <li><a href='blog.html'>Blog List</a></li>
-										<li><a href='blog-single.html'>Blog Single</a></li>
-                                    </ul>
-                                </li> 
-								<li><a href='404.html'>404</a></li>
-								<li><a href='contact-us.html'>Contact</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-3'>
-						<div class='search_box pull-right'>
-							<input type='text' placeholder='Search'/>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!--/header-bottom-->
-	</header><!--/header-->
-	
+	<?php
+        include("header.php");
+    ?>
 	<section id='form'><!--form-->
 		<div class='container'>
 			<div class='row'>
 				<div class='col-sm-4 col-sm-offset-1'>
 					<div class='login-form'><!--login form-->
 						<h2>Login to your account</h2>
-						<form action='#'>
-							<input type='text' placeholder='Name' />
-							<input type='email' placeholder='Email Address' />
+						<form action='#' method="post">
+							<input name="name" type='text' placeholder='ID' />
+							<input name="password" type='password' placeholder='Password' />
 							<span>
-								<input type='checkbox' class='checkbox'> 
+								<input name="keep_sign_in" type='checkbox' class='checkbox'> 
 								Keep me signed in
 							</span>
-							<button type='submit' class='btn btn-default'>Login</button>
+							<button name="btsubmit" type='submit' class='btn btn-default'>Login</button>
 						</form>
 					</div><!--/login form-->
 				</div>
@@ -196,7 +102,7 @@ else
 					<div class='signup-form'><!--sign up form-->
 						<h2>New User Signup!</h2>
 						<form action='#' method="post">
-							<input id='r_name' name='r_name' type='text' placeholder='Name'/>
+							<input id='r_name' name='r_name' type='text' placeholder='ID'/>
 							<input id='r_email' name='r_email' type='email' placeholder='Email Address'/>
 							<input name='r_password' type='password' placeholder='Password'/>
 							<input name='r_repassword' type='password' placeholder='Confirm Password'/>
@@ -207,165 +113,11 @@ else
 			</div>
 		</div>
 	</section><!--/form-->
+	<?php
+        include("footer.php");
+    ?>
 	
 	
-	<footer id='footer'><!--Footer-->
-		<div class='footer-top'>
-			<div class='container'>
-				<div class='row'>
-					<div class='col-sm-2'>
-						<div class='companyinfo'>
-							<h2><span>e</span>-shopper</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
-						</div>
-					</div>
-					<div class='col-sm-7'>
-						<div class='col-sm-3'>
-							<div class='video-gallery text-center'>
-								<a href='#'>
-									<div class='iframe-img'>
-										<img src='images/home/iframe1.png' alt='' />
-									</div>
-									<div class='overlay-icon'>
-										<i class='fa fa-play-circle-o'></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class='col-sm-3'>
-							<div class='video-gallery text-center'>
-								<a href='#'>
-									<div class='iframe-img'>
-										<img src='images/home/iframe2.png' alt='' />
-									</div>
-									<div class='overlay-icon'>
-										<i class='fa fa-play-circle-o'></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class='col-sm-3'>
-							<div class='video-gallery text-center'>
-								<a href='#'>
-									<div class='iframe-img'>
-										<img src='images/home/iframe3.png' alt='' />
-									</div>
-									<div class='overlay-icon'>
-										<i class='fa fa-play-circle-o'></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-						
-						<div class='col-sm-3'>
-							<div class='video-gallery text-center'>
-								<a href='#'>
-									<div class='iframe-img'>
-										<img src='images/home/iframe4.png' alt='' />
-									</div>
-									<div class='overlay-icon'>
-										<i class='fa fa-play-circle-o'></i>
-									</div>
-								</a>
-								<p>Circle of Hands</p>
-								<h2>24 DEC 2014</h2>
-							</div>
-						</div>
-					</div>
-					<div class='col-sm-3'>
-						<div class='address'>
-							<img src='images/home/map.png' alt='' />
-							<p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		
-		<div class='footer-widget'>
-			<div class='container'>
-				<div class='row'>
-					<div class='col-sm-2'>
-						<div class='single-widget'>
-							<h2>Service</h2>
-							<ul class='nav nav-pills nav-stacked'>
-								<li><a href=''>Online Help</a></li>
-								<li><a href=''>Contact Us</a></li>
-								<li><a href=''>Order Status</a></li>
-								<li><a href=''>Change Location</a></li>
-								<li><a href=''>FAQ’s</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-2'>
-						<div class='single-widget'>
-							<h2>Quock Shop</h2>
-							<ul class='nav nav-pills nav-stacked'>
-								<li><a href=''>T-Shirt</a></li>
-								<li><a href=''>Mens</a></li>
-								<li><a href=''>Womens</a></li>
-								<li><a href=''>Gift Cards</a></li>
-								<li><a href=''>Shoes</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-2'>
-						<div class='single-widget'>
-							<h2>Policies</h2>
-							<ul class='nav nav-pills nav-stacked'>
-								<li><a href=''>Terms of Use</a></li>
-								<li><a href=''>Privecy Policy</a></li>
-								<li><a href=''>Refund Policy</a></li>
-								<li><a href=''>Billing System</a></li>
-								<li><a href=''>Ticket System</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-2'>
-						<div class='single-widget'>
-							<h2>About Shopper</h2>
-							<ul class='nav nav-pills nav-stacked'>
-								<li><a href=''>Company Information</a></li>
-								<li><a href=''>Careers</a></li>
-								<li><a href=''>Store Location</a></li>
-								<li><a href=''>Affillate Program</a></li>
-								<li><a href=''>Copyright</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class='col-sm-3 col-sm-offset-1'>
-						<div class='single-widget'>
-							<h2>About Shopper</h2>
-							<form action='#' class='searchform'>
-								<input type='text' placeholder='Your email address' />
-								<button type='submit' class='btn btn-default'><i class='fa fa-arrow-circle-o-right'></i></button>
-								<p>Get the most recent updates from <br />our site and be updated your self...</p>
-							</form>
-						</div>
-					</div>
-					
-				</div>
-			</div>
-		</div>
-		
-		<div class='footer-bottom'>
-			<div class='container'>
-				<div class='row'>
-					<p class='pull-left'>Copyright © 2013 E-SHOPPER Inc. All rights reserved.</p>
-					<p class='pull-right'>Designed by <span><a target='_blank' href='http://www.themeum.com'>Themeum</a></span></p>
-				</div>
-			</div>
-		</div>
-		
-	</footer><!--/Footer-->
 	
 
   
