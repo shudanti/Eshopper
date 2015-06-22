927fb00812e584113c12650c98009ca967775d72
shudanti <shudanti@gmail.com>
2015-06-22 14:34:48 +0700

    sidebar

--- header.php
+++ header.php
@@ -63,8 +63,8 @@ echo "<header id='header'><!--header-->
 							<ul class='nav navbar-nav'>
 								<li><a href=''><i class='fa fa-user'></i> Account</a></li>
 								<li><a href=''><i class='fa fa-star'></i> Wishlist</a></li>
-								<li><a href='checkout.html'><i class='fa fa-crosshairs'></i> Checkout</a></li>
-								<li><a href='cart.html'><i class='fa fa-shopping-cart'></i> Cart</a></li>
+								<li><a href='checkout.php'><i class='fa fa-crosshairs'></i> Checkout</a></li>
+								<li><a href='cart.php'><i class='fa fa-shopping-cart'></i> Cart</a></li>
 								<li><a href='login.php' class='active'><i class='fa fa-lock'></i> Login</a></li>
 							</ul>
 						</div>
@@ -90,21 +90,21 @@ echo "<header id='header'><!--header-->
 								<li><a href='index.php'>Home</a></li>
 								<li class='dropdown'><a href='#'>Shop<i class='fa fa-angle-down'></i></a>
                                     <ul role='menu' class='sub-menu'>
-                                        <li><a href='shop.html'>Products</a></li>
-										<li><a href='product-details.html'>Product Details</a></li> 
-										<li><a href='checkout.html'>Checkout</a></li> 
-										<li><a href='cart.html'>Cart</a></li> 
+                                        <li><a href='shop.php'>Products</a></li>
+										<li><a href='product-details.php'>Product Details</a></li> 
+										<li><a href='checkout.php'>Checkout</a></li> 
+										<li><a href='cart.php'>Cart</a></li> 
 										<li><a href='login.php' class='active'>Login</a></li> 
                                     </ul>
                                 </li> 
 								<li class='dropdown'><a href='#'>Blog<i class='fa fa-angle-down'></i></a>
                                     <ul role='menu' class='sub-menu'>
                                         <li><a href='blog.html'>Blog List</a></li>
-										<li><a href='blog-single.html'>Blog Single</a></li>
+										<li><a href='blog-single.php'>Blog Single</a></li>
                                     </ul>
                                 </li> 
 								<li><a href='404.html'>404</a></li>
-								<li><a href='contact-us.html'>Contact</a></li>
+								<li><a href='contact-us.php'>Contact</a></li>
 							</ul>
 						</div>
 					</div>
