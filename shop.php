97f205c40c8a71ae9aa292860a83d88adae77858
shudanti <shudanti@gmail.com>
2015-06-22 16:26:29 +0700

    fix shop

--- shop.php
+++ shop.php
@@ -74,13 +74,6 @@
     													<p>".mysql_result($sel,$x,"Name")."</p>
     													<a href='#' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Add to cart</a>
     												</div>
-    												<div class='product-overlay'>
-    													<div class='overlay-content'>
-    														<h2>".mysql_result($sel,$x,"price")."$</h2>
-    														<p>".mysql_result($sel,$x,"Name")."</p>
-    														<a href='#' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Add to cart</a>
-    													</div>
-    												</div>
     										</div>
     										<div class='choose'>
     											<ul class='nav nav-pills nav-justified'>
